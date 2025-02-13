1f4f1538992d48f0c8b3c15a68b10a15b90425e2
bulletbolt6741 <153509572+bulletbolt6741@users.noreply.github.com>
2025-02-13 21:59:57 +0000

    Create Question_1.pl

---
+++ cs172_lab3/Question_1.pl
@@ -0,0 +1,22 @@
+/*
+Below is the output from various queries made to a Prolog program:
+    fruit(apple).
+      true.
+    fruit(orange).
+      true.
+    fruit(banana).
+      true.
+    fruit(grape).
+      true.
+    fruit(hamburger).
+      false.
+    fruit(cheese).
+      false.
+Write a program that responds exactly as above.
+*/
+fruit(apple).
+fruit(orange).
+fruit(banana).
+fruit(grape).
+burger(hamburger).
+dairy(cheese).
